f8a988febc4cd35ec0e1649a8d15770299ce3e8b
Muhammad Ali <softeng.aliijaz@gmail.com>
2025-12-23 03:25:48 +0500

    feat: Introduce user management API with JWT authentication and role-based access control.

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { authorizeRoles } from "../middlewares/authorizeRoles.js";
+import { authorizeRoles, checkAuth } from "../middlewares/auth.middleware.js";
 import {
   getAllUsers,
   createNewUser,
@@ -11,11 +11,21 @@ import {
 
 const router = express.Router();
 
-router.get("/", authorizeRoles("admin"), getAllUsers);
-router.post("/", authorizeRoles("admin"), createNewUser);
-router.get("/:id", authorizeRoles("admin"), getSingleUser);
-router.put("/:id", authorizeRoles("admin"), updateUser);
-router.delete("/:id", authorizeRoles("admin"), deleteUser);
-router.get("/profile", authorizeRoles("admin", "user"), getUserProfile);
+router.get("/", checkAuth, authorizeRoles("admin"), getAllUsers);
+
+router.post("/", checkAuth, authorizeRoles("admin"), createNewUser);
+
+router.get("/:id", checkAuth, authorizeRoles("admin"), getSingleUser);
+
+router.put("/:id", checkAuth, authorizeRoles("admin"), updateUser);
+
+router.delete("/:id", checkAuth, authorizeRoles("admin"), deleteUser);
+
+router.get(
+  "/profile",
+  checkAuth,
+  authorizeRoles("admin", "user"),
+  getUserProfile
+);
 
 export default router;
